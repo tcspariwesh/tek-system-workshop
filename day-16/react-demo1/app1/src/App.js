6b490d8580f529c0602bf742204aee50428270be
tcspariwesh <tcspariwesh@gmail.com>
2026-04-01 16:14:59 +0530

    initial react app

---
+++ day-16/react-demo1/app1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { //component
+  return (
+    <div className="App">
+      <input />
+    </div>
+  );
+}
+
+export default App;
