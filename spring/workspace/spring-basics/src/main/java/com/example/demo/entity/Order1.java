7ab949cbd0bde69c7064861fe00b01045492624c
tcspariwesh <tcspariwesh@gmail.com>
2026-04-10 12:10:11 +0530

    basic validations

--- spring/workspace/spring-basics/src/main/java/com/example/demo/entity/Order1.java
+++ spring/workspace/spring-basics/src/main/java/com/example/demo/entity/Order1.java
@@ -1,8 +1,13 @@
 package com.example.demo.entity;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class Order1 {
 	private int id;
+	@NotBlank
 	private String item;
+	@Min(value = 1)
 	private float price;
 
 	public int getId() {
